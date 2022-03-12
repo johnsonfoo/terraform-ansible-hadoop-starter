c9234b049968a867b62a7480f6ee6a185c8f91cf
johnsonfoo <johnsonfoo18@gmail.com>
2022-03-12 20:57:39 +0800

    Add script to provision AWS EC2 instance

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.5"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami           = "ami-02f47fa62c613afb4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleEC2Instance"
+  }
+}
+
