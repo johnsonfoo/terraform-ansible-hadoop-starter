f4ad95a9ac5f88740c9764f8a8a9029b2210fa67
johnsonfoo <johnsonfoo18@gmail.com>
2022-03-13 01:12:41 +0800

    Attach key pair to EC2 instances to enable SSH connection

--- main.tf
+++ main.tf
@@ -18,9 +18,21 @@ resource "aws_instance" "ec2_instance" {
   ami           = "ami-02f47fa62c613afb4"
   instance_type = "t2.micro"
   count         = var.instance_count
+  key_name      = "aws_key"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = self.public_ip
+    private_key = file("${var.private_key_path}")
+  }
 
   tags = {
     Name = "${var.instance_name}-${count.index + 1}"
   }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = file("${var.public_key_path}")
+}
