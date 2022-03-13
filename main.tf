27930ff30036a9d3d5ede2934dbe426249fc7a47
johnsonfoo <johnsonfoo18@gmail.com>
2022-03-13 17:43:16 +0800

    Add security group resource to enable incoming traffic to EC2 instances

--- main.tf
+++ main.tf
@@ -15,10 +15,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-02f47fa62c613afb4"
-  instance_type = "t2.micro"
-  count         = var.instance_count
-  key_name      = "aws_key"
+  ami                    = "ami-02f47fa62c613afb4"
+  instance_type          = "t2.micro"
+  count                  = var.instance_count
+  key_name               = "aws_key"
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
 
   connection {
     type        = "ssh"
@@ -35,4 +36,29 @@ resource "aws_instance" "ec2_instance" {
 resource "aws_key_pair" "deployer" {
   key_name   = "aws_key"
   public_key = file("${var.public_key_path}")
+}
+
+resource "aws_security_group" "ec2_security_group" {
+  ingress = [
+    {
+      description      = ""
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0", ]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    description      = ""
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
